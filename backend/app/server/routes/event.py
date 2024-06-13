57a7e2773cc0dbc001874af4279fed64ca4992ba
Mahmudul Hasan <mahmudul.hhh@gmail.com>
2024-06-13 16:59:58 +0600

    news-events

--- backend/app/server/routes/event.py
+++ backend/app/server/routes/event.py
@@ -13,23 +13,32 @@ from server.models.event import (
     UpdateEventsBaseModel
 )
 
-
 from server.middlewares.auth import (
     check_token
 )
+
 from server.models.user import (
     UserSchema,
     ResponseModel,
     ErrorResponseModel,
-
 )
 
 router = APIRouter()
 
+
+# getting all events
+@router.get("/all", response_description="Events viewed")
+@check_token
+async def get_event_(request: Request, response: Response,):
+    events = await get_events()
+    if events:
+        return ResponseModel("Events are viewed successfully")
+    return ResponseModel(events, "Couldn't find events")
+
 # Creating event
 @router.post("/add", response_description="event has been added")
-# @check_token
-async def create_event(event: EventsBaseModel = Body(...)):
+@check_token
+async def create_event(request: Request, response: Response, event: EventsBaseModel = Body(...)):
     events = jsonable_encoder(event)
     new_event = await add_event(events)
     return ResponseModel(new_event, "Event added successfully")
@@ -38,27 +47,16 @@ async def create_event(event: EventsBaseModel = Body(...)):
 # getting event by id
 @router.get("/{event_id}")
 @check_token
-async def get_event_id(event_id):
+async def get_event_id(request: Request, response: Response, event_id):
     event = await get_event_by_id(id=event_id)
     if event:
-        return ResponseModel("Event viewed successfully")
+        return ResponseModel(event, "Event viewed successfully")
     return ResponseModel(event, "Couldn't find this event")
-
-
-# getting all events
-@router.get("/all", response_description="Events viewed")
-@check_token
-async def get_event_():
-    events = await get_events()
-    if events:
-        return ResponseModel("Events are viewed successfully")
-    return ResponseModel(events, "Couldn't find events")
-
 
 # updating events by event id
 @router.put("/update/{event_id}")
 @check_token
-async def update_event_(event_id, updated_event: UpdateEventsBaseModel = Body(...)):
+async def update_event_(request: Request, response: Response, event_id, updated_event: UpdateEventsBaseModel = Body(...)):
     events = {k: v for k, v in updated_event.dict().items() if v is not None}
     updated = await update_event(id=events, updated_data=events)
     if updated:
