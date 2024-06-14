230f9ffdcc7de9e94d60f643f4d469b851747757
Mahmudul Hasan <mahmudul.hhh@gmail.com>
2024-06-14 22:08:16 +0600

    event-reg

--- backend/app/server/models/event.py
+++ backend/app/server/models/event.py
@@ -1,6 +1,15 @@
 from typing import Optional
 from pydantic import BaseModel, EmailStr, Field
 
+
+'''
+public:   0000
+admin:    0001
+student:  0010
+alumni:   0100
+teacher:  1000
+'''
+
 class EventsBaseModel(BaseModel):
     event_title:str=Field(...)
     date:str=Field(...)
@@ -8,25 +17,7 @@ class EventsBaseModel(BaseModel):
     venue:str=Field(...)
     date_and_time:str=Field(...)
     photo:str=Field(...)
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "event_title": "Code Samurai 2024",
-                "date": "07-05-2024",
-                "description": "something about the event",
-                "venue":"CSE,DU",
-                "date_and_time" : "20-05-2024 10:00 AM",
-                "photo": "photo url"
-            }
-        }
-
-class UpdateEventsBaseModel(BaseModel):
-    event_title:Optional[str]
-    date:Optional[str]
-    description:Optional[str]
-    venue:Optional[str]
-    date_and_time:Optional[str]
-    photo:Optional[str]
+    allowed_roles: int = Field(...)
     class Config:
         json_schema_extra = {
             "example": {
@@ -36,6 +27,81 @@ class UpdateEventsBaseModel(BaseModel):
                 "venue":"CSE,DU",
                 "date_and_time" : "20-05-2024 10:00 AM",
                 "photo": "photo url",
+                "allowed_roles": 1
+            }
+        }
+
+
+class UpdateEventsBaseModel(BaseModel):
+    event_title:Optional[str]
+    date:Optional[str]
+    description:Optional[str]
+    venue:Optional[str]
+    date_and_time:Optional[str]
+    photo:Optional[str]
+    allowed_roles: Optional[int]
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "event_title": "Code Samurai 2024",
+                "date": "07-05-2024",
+                "description": "something about the event",
+                "venue":"CSE,DU",
+                "date_and_time" : "20-05-2024 10:00 AM",
+                "photo": "photo url",
+                "allowed_roles": 1
+            }
+        }
+
+
+# User form Data
+class UserFormModel(BaseModel):
+    event_id: str = Field(...)
+    user_id: str = Field(...)
+    name: str = Field(...)
+    email: EmailStr = Field(...)
+    phone: str = Field(...)
+    amount: int = Field(...)
+    trxId: str = Field(...)
+    comment: Optional[str] = Field(...)
+    status: Optional[str] = Field("pending")
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "event_id": "60d0fe0b9d1d1f0c1c9c8c6c",
+                "user_id": "60d0fe0b9d1d1f0c1c9c8c6c",
+                "name": "John Doe",
+                "email": "abc@gmail.com",
+                "phone": "1234567890",
+                "amount": 1000,
+                "trxId": "123456",
+                "comment": "I am interested in this event",
+                "status": "pending"
+            }
+        }
+
+# User form Update Data
+class UpdateUserFormModel(BaseModel):
+    name: Optional[str]
+    email: Optional[EmailStr]
+    phone: Optional[str]
+    amount: Optional[int]
+    trxId: Optional[str]
+    comment: Optional[str]
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "event_id": "60d0fe0b9d1d1f0c1c9c8c6c",
+                "user_id": "60d0fe0b9d1d1f0c1c9c8c6c",
+                "name": "John Doe",
+                "email": "abc@gmail.com",
+                "phone": "1234567890",
+                "amount": 1000,
+                "trxId": "123456",
+                "comment": "I am interested in this event",
+                "status": "pending"
             }
         }
 
--- backend/app/server/routes/event.py
+++ backend/app/server/routes/event.py
@@ -6,12 +6,16 @@ from server.controller.event import (
     get_events,
     add_event,
     update_event,
-    event_delete
+    event_delete,
+    event_register,
+    get_event_registration_list
 )
 
 from server.models.event import (
     EventsBaseModel,
-    UpdateEventsBaseModel
+    UpdateEventsBaseModel,
+    UserFormModel,
+    UpdateUserFormModel
 )
 
 from server.middlewares.auth import (
@@ -25,6 +29,27 @@ from server.models.user import (
 )
 
 router = APIRouter()
+
+'''
+public:   0000
+admin:    0001
+student:  0010
+alumni:   0100
+teacher:  1000
+'''
+
+def role_access(bit, role):
+    if bit == 0:
+        return True;
+    if bit&1 and role == "admin":
+        return True
+    if bit&(1<<1) and role == "student":
+        return True
+    if bit&(1<<2) and role == "alumni":
+        return True
+    if bit&(1<<3) and role == "teacher":
+        return True
+    return False
 
 
 # getting all events
@@ -87,26 +112,44 @@ async def update_event_(request: Request, response: Response, event_id, updated_
         print("error:"+e)
         return ErrorResponseModel("An error occurred", 400)
 
-# Event Registration
-# @router.post("/register/{event_id}")
-# @check_token
-# async def register_event(request: Request, response: Response, event_id):
-#     try:
-#         user = request.state.user
-#         event = await get_event_by_id(id=event_id)
-#         if event:
-#             return ResponseModel(
-#                 "event with ID: {} event registered successful".format(event_id),
-#                 "event registered successfully",
-#             )
-#         return ErrorResponseModel(
-#             "An error occurred",
-#             404,
-#         )
-#     except Exception as e:
-#         print("error: "+e)
-#         return ErrorResponseModel("An error occurred", 400)
+# Event Registration For CSEDU People
+@router.post("/register/{event_id}")
+@check_token
+async def register_event(request: Request, response: Response, event_id: str, form: UserFormModel = Body(...)):
+    try:
+        event = await get_event_by_id(id=event_id)
+        if not event:
+            return ErrorResponseModel("Event not found", 404)
+            
+        user = request.state.user
+        if not role_access(event["allowed_roles"], user["role"]):
+            return ErrorResponseModel("You are not allowed to register for this event", 403, "Unauthorized Access")
 
+        form = jsonable_encoder(form)
+        success = await event_register(event_id, form)
+        if not success:
+            return ErrorResponseModel("An error occurred", 400, "An error occurred")
+        return ResponseModel("Event registered successfully", "Event registered successfully")
+    except Exception as e:
+        print("error: ", e)
+        return ErrorResponseModel("An error occurred", 400, e)
+
+# getting event registration list
+@router.get("/register/list/{event_id}")
+@check_token
+async def get_registration_list(request: Request, response: Response, event_id):
+    try:
+        event = await get_event_by_id(id=event_id)
+        if not event:
+            return ErrorResponseModel("Event not found", 404, "Event not found")
+        user = request.state.user
+        if not user["role"] == "admin":
+            return ErrorResponseModel("You are not allowed to view this list", 403, "Unauthorized Access")
+        registration_list = await get_event_registration_list(event_id)
+        return ResponseModel(registration_list, "Event registration list viewed successfully")
+    except Exception as e:
+        print("error: ", e)
+        return ErrorResponseModel("An error occurred", 400, e)
 
 # deleting event by id
 @router.delete("/delete/{event_id}")
