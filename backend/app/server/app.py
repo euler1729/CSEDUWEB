034086bc22b95cf4625b832de540a41c2c982790
Mahmudul Hasan <mahmudul.hhh@gmail.com>
2024-06-18 14:39:17 +0600

    message

--- backend/app/server/app.py
+++ backend/app/server/app.py
@@ -10,6 +10,7 @@ from server.routes.alumni import router as AlumniRouter
 from server.routes.student import router as StudentRouter
 from server.routes.teacher import router as TeacherRouter
 from server.routes.stats import router as StatsRouter
+from server.routes.message import router as MessageRouter
 
 app = FastAPI()
 
@@ -22,7 +23,7 @@ app.include_router(AlumniRouter, tags=["Alumni"], prefix="/alumni")
 app.include_router(StudentRouter, tags=["Student"], prefix="/student")
 app.include_router(TeacherRouter, tags=["Teacher"], prefix="/teacher")
 app.include_router(StatsRouter, tags=["Stats"], prefix="/stats")
-
+app.include_router(MessageRouter, tags=["Message"], prefix="/message")
 
 # Enable CORS
 app.add_middleware(
